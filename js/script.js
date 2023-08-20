c8f0b63f3990adf15b76cc61b4502b36bf2ba1bb
Josh Paul <timeforce@proton.me>
2023-08-20 14:41:56 -0600

    added pseudo code.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// PSEUDO CODE
+/* 
+A quiz page with start button. Press the button and start timer of 100 seconds and launch first question. 
+The first question multiple choice, if correct says correct with timer delay for next question, or incorrect and subtracts from time.
+Second question.
+Third.
+Fourth.
+Fifth.
+End game screen to type initials and save score to local storage and persist data. Start button to play again.
+*/
